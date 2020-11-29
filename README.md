5fa11277c75d94c72a9e2b4b7a4b6cfe54114ddf
Spencer Bertsch <spencerbertsch99@gmail.com>
2020-11-28 18:03:22 -0800

    add __init__ for flask app

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # climate_website
 
-https://www.youtube.com/results?search_query=sentdex+django+tutorial
+Website to promote climate justice. 
 
-https://www.youtube.com/watch?v=UmljXZIypDc&list=PL-osiE80TeTtoQCKZ03TU5fNfx2UY6U4p&ab_channel=CoreySchafer
+Made by three friends who love this planet! 
---
+++ __init__.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+This file contains a flask script which represents the website
+spencerbertsch.com
+"""
+
+# Imports
+from flask import (Flask, render_template, request, redirect, jsonify,
+                   url_for, flash)
+
+from flask import session as login_session
+import random
+import string
+
+import httplib2
+import json
+from flask import make_response
+import requests
+import os
+
+app = Flask(__name__)
+
+# Web Page #1 - Homepage - Website homepage
+@app.route('/')
+@app.route('/home')
+def showHomepage():
+    return render_template('homepage.html')
+
+# Web Page #2 - Article1 - First article 
+@app.route('/article1')
+def showArticle1():
+    return render_template('article1.html')
+
+if __name__ == '__main__':
+    app.secret_key = 'super_secret_key'
+    app.debug = True
+    app.run(host='0.0.0.0', port=5000)
